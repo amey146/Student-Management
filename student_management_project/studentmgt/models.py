1e4c899c3de6a63a1ed7cc5e41aa5947fd37a75e
amey146 <108222636+amey146@users.noreply.github.com>
2024-08-06 21:26:49 +0530

    Added course_student reln

--- student_management_project/studentmgt/models.py
+++ student_management_project/studentmgt/models.py
@@ -1,6 +1,8 @@
 
 from django.db import models
 
+from coursemgt.models import Course
+
 class Student(models.Model):
     st_id = models.TextField(primary_key=True)
     fname = models.CharField(max_length=255)
@@ -11,3 +13,8 @@ class Student(models.Model):
     pr_email = models.EmailField()
     phone = models.CharField(max_length=20)
     address = models.TextField()
+    courses = models.ManyToManyField(Course, related_name='students')
+
+    def __str__(self):
+        return f"{self.fname} {self.lname}"
+
--- student_management_project/studentmgt/views.py
+++ student_management_project/studentmgt/views.py
@@ -1,44 +1,49 @@
 from django.shortcuts import redirect, render, HttpResponse
 
+from coursemgt.models import Course
 from studentmgt.models import Student
 from django.contrib import messages
 
 # Create your views here.
 def student_home(request):
-    std=Student.objects.all()
-    return render(request, "studentmgt/student_home.html",{'std':std})
+    std = Student.objects.all()
+    return render(request, "studentmgt/student_home.html", {'std': std})
+
 
 def student_add(request):
-    if request.method=='POST':
-        #retrive the user inputs
+    if request.method == 'POST':
+        # Retrieve the user inputs
         st_id = request.POST.get("st_id")
         fname = request.POST.get("fname")
         lname = request.POST.get("lname")
         dob = request.POST.get("dob")
-        gender=request.POST.get("gender")
+        gender = request.POST.get("gender")
         st_email = request.POST.get("st_email")
-        pr_email=request.POST.get("pr_email")
+        pr_email = request.POST.get("pr_email")
         phone = request.POST.get("phone")
         address = request.POST.get("address")
-        
+        course_ids = request.POST.getlist("courses")
+
         # Check if the student with the given st_id already exists
         if Student.objects.filter(st_id=st_id).exists():
             error_message = 'Error! A student with this Roll ID already exists.'
-            return render(request, "studentmgt/student_add.html", {'error_message': error_message})
+            courses = Course.objects.all()
+            return render(request, "studentmgt/student_add.html", {'error_message': error_message, 'courses': courses})
 
-        s=Student()
-        s.st_id=st_id
-        s.fname=fname
-        s.lname=lname
-        s.dob=dob
-        s.gender=gender
-        s.st_email=st_email
-        s.pr_email=pr_email
-        s.phone=phone
-        s.address=address
+        # Create and save the student
+        s = Student(st_id=st_id, fname=fname, lname=lname, dob=dob, gender=gender,
+                    st_email=st_email, pr_email=pr_email, phone=phone, address=address)
         s.save()
+
+        # Assign selected courses to the student
+        for course_id in course_ids:
+            course = Course.objects.get(c_id=course_id)
+            s.courses.add(course)
+
         return redirect('/studentmgt')
-    return render(request,"studentmgt/student_add.html",{})
+
+    courses = Course.objects.all()
+    return render(request, "studentmgt/student_add.html", {'courses': courses})
 
 
 
@@ -47,30 +52,41 @@ def student_delete(request,st_id):
     s.delete()
     return redirect("/studentmgt")
 
-def student_update(request,st_id):
-    std=Student.objects.get(pk=st_id)
-    return render(request,"studentmgt/student_update.html",{'std':std})
+def student_update(request, st_id):
+    std = Student.objects.get(pk=st_id)
+    courses = Course.objects.all()
+    return render(request, "studentmgt/student_update.html", {'std': std, 'courses': courses})
 
-def student_do_update(request,st_id):
+def student_do_update(request, st_id):
+    if request.method == 'POST':
         st_id = request.POST.get("st_id")
         fname = request.POST.get("fname")
         lname = request.POST.get("lname")
         dob = request.POST.get("dob")
-        gender=request.POST.get("gender")
+        gender = request.POST.get("gender")
         st_email = request.POST.get("st_email")
-        pr_email=request.POST.get("pr_email")
+        pr_email = request.POST.get("pr_email")
         phone = request.POST.get("phone")
         address = request.POST.get("address")
+        course_ids = request.POST.getlist("courses")
 
-        std=Student.objects.get(pk=st_id)
-        std.st_id=st_id
-        std.fname=fname
-        std.lname=lname
-        std.dob=dob
-        std.gender=gender
-        std.st_email=st_email
-        std.pr_email=pr_email
-        std.phone=phone
-        std.address=address
+        std = Student.objects.get(pk=st_id)
+        std.fname = fname
+        std.lname = lname
+        std.dob = dob
+        std.gender = gender
+        std.st_email = st_email
+        std.pr_email = pr_email
+        std.phone = phone
+        std.address = address
         std.save()
+
+        # Clear existing courses and add the selected ones
+        std.courses.clear()
+        for course_id in course_ids:
+            course = Course.objects.get(c_id=course_id)
+            std.courses.add(course)
+
         return redirect("/studentmgt")
+
+    return redirect("/studentmgt")
